79ec8cacdaeff1ff3ff0080eb80747ea2aa9985c
MasoodGhauri <mmasoodghauri@gmail.com>
2023-12-12 20:15:52 +0500

    Starter Code

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+require("dotenv").config();
+
+app.use(cors());
+
+// cors settings
+app.options("*", cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,PUT,POST,DELETE,PATCH,OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type,Authorization,Content-Length,X-Requested-With"
+  );
+  next();
+});
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.log(err));
+
+app.listen(3001, () => {
+  console.log("Server is running on port 3001");
+});
