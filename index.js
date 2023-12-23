71613f3a113cba0409781cc90a2d05d3b08e2014
MasoodGhauri <mmasoodghauri@gmail.com>
2023-12-23 12:13:01 +0500

    userCheck removed, events refined

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-
-// routes
-// const videosessionRoute = require("./Routes/Masood/VideoSession");
-
-const app = express();
-app.use(express.json());
 require("dotenv").config();
+const express = require("express");
+const http = require("http");
+const app = express();
+const server = http.createServer(app);
+const cors = require("cors");
+const socket = require("socket.io");
 
-const http = require("http").createServer(app);
-const io = require("socket.io")(http, {
+const io = socket(server, {
   cors: {
     origin: "*",
     methods: ["GET", "POST"],
@@ -39,101 +35,91 @@ mongoose
   .then(() => console.log("DB Connected"))
   .catch((err) => console.log(err));
 
-// app.use("/api/videosession", videosessionRoute);
+// used vaiables
+const users = {};
+const socketsList = {};
 
-let socketsList = {};
 io.on("connection", (socket) => {
-  console.log("a user connected: " + socket.id);
+  socket.on("b-join room", (roomID) => {
+    if (users[roomID]) {
+      // Check if the current socket is not already in the room
+      if (!users[roomID].includes(socket.id)) {
+        users[roomID].push(socket.id);
+      }
+    } else {
+      users[roomID] = [socket.id];
+    }
 
-  socket.on("disconnect", () => {
-    socket.disconnect();
-    console.log("user disconnected");
+    socketsList[socket.id] = roomID;
+
+    const usersInThisRoom = users[roomID].filter((id) => id !== socket.id);
+
+    console.log(usersInThisRoom);
+    socket.emit("f-users joined", usersInThisRoom);
   });
 
-  // check if the user is not already present in the room
-  socket.on("b-check user", ({ roomId, userName }) => {
-    console.log("checking user presence... name " + userName);
-
-    io.sockets
-      .in(roomId)
-      .allSockets()
-      .then((users) => {
-        const isUserPresent = Array.from(users).some((clientSocketId) => {
-          return (
-            socketsList[clientSocketId] &&
-            socketsList[clientSocketId].userName === userName
-          );
-        });
-
-        if (isUserPresent) {
-          socket.emit("f-user present");
-          console.log("user already present in this room");
-        } else {
-          socket.emit("f-user not present");
-          console.log("user not present in any room");
-        }
-      })
-      .catch((error) => {
-        console.error("Error checking user presence:", error);
-        socket.emit("f-error checking user presence");
-      });
-  });
-
-  // add user to the room as he is already not present
-  socket.on("b-user join", ({ roomId, userName }) => {
-    socket.join(roomId);
-    socketsList[socket.id] = { userName, video: true, audio: true };
-
-    io.sockets
-      .in(roomId)
-      .allSockets()
-      .then((roomClients) => {
-        try {
-          const allUsers = [];
-          Array.from(roomClients).forEach((clientSocketId) => {
-            allUsers.push({
-              userId: clientSocketId,
-              info: socketsList[clientSocketId],
-            });
-          });
-
-          socket.emit("f-users joined", allUsers);
-        } catch (e) {
-          console.error("Error joining room", e);
-          socket.emit("f-error joining room");
-        }
-      })
-      .catch((error) => {
-        console.error("Error getting room clients", error);
-        socket.emit("f-error joining room");
-      });
-  });
-
-  // when someone want to connect
   socket.on("b-request connect", ({ userToConnect, from, signal }) => {
     io.to(userToConnect).emit("f-get request", {
-      signal,
-      from,
-      info: socketsList[socket.id], // sending info (audio && video) of user who want to connect
+      signal: signal,
+      from: from,
     });
   });
 
-  // connection request accepted
-  socket.on("b-accept connect", ({ signal, to }) => {
-    io.to(to).emit("f-accepted connect", {
-      signal,
-      answerId: socket.id,
+  socket.on("b-accept connect", ({ from, signal }) => {
+    io.to(from).emit("f-accepted connect", {
+      signal: signal,
+      id: socket.id,
     });
   });
+
+  // disconnect not working properly
+  // socket.on("disconnect", () => {
+  //   const roomID = socketsList[socket.id];
+  //   let room = users[roomID];
+  //   if (room) {
+  //     room = room.filter((id) => id !== socket.id);
+  //     users[roomID] = room;
+  //   }
+  //   socket.broadcast.emit("user left", socket.id);
+  // });
+
+  // socket.on("disconnect", () => {
+  //   console.log("left : "+socket.id);
+  //   const roomID = socketsList[socket.id];
+
+  //   if (roomID) {
+  //     let room = users[roomID];
+
+  //     if (room) {
+  //       // Filter out the disconnected socket ID from the room
+  //       room = room.filter((id) => id !== socket.id);
+  //       users[roomID] = room;
+
+  //       // Notify other users in the room about the disconnection
+  //       socket.broadcast.to(roomID).emit("user left", socket.id);
+  //     }
+  //   }
+  // });
+
+  // socket.on("disconnect", () => {
+  //   console.log("left: " + socket.id);
+  //   const roomID = socketsList[socket.id];
+
+  //   if (roomID) {
+  //     let room = users[roomID];
+
+  //     if (room) {
+  //       // Filter out the disconnected socket ID from the room
+  //       room = room.filter((id) => id !== socket.id);
+  //       users[roomID] = room;
+
+  //       // Notify other users in the room about the disconnection
+  //       io.to(roomID).emit("user left", socket.id);
+  //     }
+  //   }
+  // });
 });
 
-http.listen(3001, () => {
-  console.log("Server is running on port 3001");
+server.listen(3001, () => {
+  console.log("Server started at 3001");
 });
-
-// // Handle shutdown gracefully
-// process.on("SIGINT", () => {
-//   console.log("Shutting down server");
-//   app.server.close();
-//   process.exit();
-// });
